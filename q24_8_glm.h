c6515ef43f666978a614aebee0e5500fdc442e96
Mehmet Suzer <mehmet.suzer610@gmail.com>
2025-12-03 22:13:48 +0100

    q24_8_glm.h and q8_24_glm.h have been added

---
+++ q24_8_glm.h
@@ -0,0 +1,13 @@
+
+#ifndef __Q24_8_GLM_H__
+#define __Q24_8_GLM_H__
+
+#include "fixed/q24_8.h"
+#include "vector/q24_8_vec.h"
+#include "quat/q24_8_quat.h"
+#include "matrix/q24_8_mat.h"
+#include "transformation/q24_8_transformation.h"
+#include "geometry/q24_8_geometry.h"
+
+#endif // __Q24_8_GLM_H__
+
---
+++ q8_24_glm.h
@@ -0,0 +1,13 @@
+
+#ifndef __Q8_24_GLM_H__
+#define __Q8_24_GLM_H__
+
+#include "fixed/q8_24.h"
+#include "vector/q8_24_vec.h"
+#include "quat/q8_24_quat.h"
+#include "matrix/q8_24_mat.h"
+#include "transformation/q8_24_transformation.h"
+#include "geometry/q8_24_geometry.h"
+
+#endif // __Q8_24_GLM_H__
+

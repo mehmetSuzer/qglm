93ee902b844b802200edcea752f5c0db1a981182
Mehmet Suzer <mehmet.suzer610@gmail.com>
2025-12-06 15:48:37 +0100

    matrix multiplication and transpose functions have been implemented

--- matrix/q_mat_mul_template.h
+++ matrix/q_mat_mul_template.h
@@ -12,8 +12,6 @@
 #include "q_mat4x3_template.h"
 #include "q_mat4_template.h"
 
-// TODO: implement functions
-
 // -------------------------------- Q_MAT2 -------------------------------- //
 
 static inline Q_MAT2 q_mat2_mul_mat2(Q_MAT2 m1, Q_MAT2 m2) 
@@ -29,19 +27,43 @@ static inline Q_MAT2 q_mat2_mul_mat2(Q_MAT2 m1, Q_MAT2 m2)
 
 static inline Q_MAT2X3 q_mat2_mul_mat2x3(Q_MAT2 m1, Q_MAT2X3 m2)
 {
-
+    return (Q_MAT2X3){{
+        q_add(q_mul(m1.xx, m2.xx), q_mul(m1.xy, m2.yx)),
+        q_add(q_mul(m1.xx, m2.xy), q_mul(m1.xy, m2.yy)),
+        q_add(q_mul(m1.xx, m2.xz), q_mul(m1.xy, m2.yz)),
+        
+        q_add(q_mul(m1.yx, m2.xx), q_mul(m1.yy, m2.yx)),
+        q_add(q_mul(m1.yx, m2.xy), q_mul(m1.yy, m2.yy)),
+        q_add(q_mul(m1.yx, m2.xz), q_mul(m1.yy, m2.yz)),
+    }};
 }
 
 static inline Q_MAT2X4 q_mat2_mul_mat2x4(Q_MAT2 m1, Q_MAT2X4 m2)
 {
-
+    return (Q_MAT2X4){{
+        q_add(q_mul(m1.xx, m2.xx), q_mul(m1.xy, m2.yx)),
+        q_add(q_mul(m1.xx, m2.xy), q_mul(m1.xy, m2.yy)),
+        q_add(q_mul(m1.xx, m2.xz), q_mul(m1.xy, m2.yz)),
+        q_add(q_mul(m1.xx, m2.xw), q_mul(m1.xy, m2.yw)),
+        
+        q_add(q_mul(m1.yx, m2.xx), q_mul(m1.yy, m2.yx)),
+        q_add(q_mul(m1.yx, m2.xy), q_mul(m1.yy, m2.yy)),
+        q_add(q_mul(m1.yx, m2.xz), q_mul(m1.yy, m2.yz)),
+        q_add(q_mul(m1.yx, m2.xw), q_mul(m1.yy, m2.yw)),
+    }};
 }
 
 // -------------------------------- Q_MAT2X3 -------------------------------- //
 
 static inline Q_MAT2 q_mat2x3_mul_mat3x2(Q_MAT2X3 m1, Q_MAT3X2 m2)
 {
-
+    return (Q_MAT2){{
+        q_add(q_add(q_mul(m1.xx, m2.xx), q_mul(m1.xy, m2.yx)), q_mul(m1.xz, m2.zx)),
+        q_add(q_add(q_mul(m1.xx, m2.xy), q_mul(m1.xy, m2.yy)), q_mul(m1.xz, m2.zy)),
+        
+        q_add(q_add(q_mul(m1.yx, m2.xx), q_mul(m1.yy, m2.yx)), q_mul(m1.yz, m2.zx)),
+        q_add(q_add(q_mul(m1.yx, m2.xy), q_mul(m1.yy, m2.yy)), q_mul(m1.yz, m2.zy)),
+    }};
 }
 
 static inline Q_MAT2X3 q_mat2x3_mul_mat3(Q_MAT2X3 m1, Q_MAT3 m2) 
@@ -59,19 +81,43 @@ static inline Q_MAT2X3 q_mat2x3_mul_mat3(Q_MAT2X3 m1, Q_MAT3 m2)
 
 static inline Q_MAT2X4 q_mat2x3_mul_mat3x4(Q_MAT2X3 m1, Q_MAT3X4 m2)
 {
-
+    return (Q_MAT2X4){{
+        q_add(q_add(q_mul(m1.xx, m2.xx), q_mul(m1.xy, m2.yx)), q_mul(m1.xz, m2.zx)),
+        q_add(q_add(q_mul(m1.xx, m2.xy), q_mul(m1.xy, m2.yy)), q_mul(m1.xz, m2.zy)),
+        q_add(q_add(q_mul(m1.xx, m2.xz), q_mul(m1.xy, m2.yz)), q_mul(m1.xz, m2.zz)),
+        q_add(q_add(q_mul(m1.xx, m2.xw), q_mul(m1.xy, m2.yw)), q_mul(m1.xz, m2.zw)),
+        
+        q_add(q_add(q_mul(m1.yx, m2.xx), q_mul(m1.yy, m2.yx)), q_mul(m1.yz, m2.zx)),
+        q_add(q_add(q_mul(m1.yx, m2.xy), q_mul(m1.yy, m2.yy)), q_mul(m1.yz, m2.zy)),
+        q_add(q_add(q_mul(m1.yx, m2.xz), q_mul(m1.yy, m2.yz)), q_mul(m1.yz, m2.zz)),
+        q_add(q_add(q_mul(m1.yx, m2.xw), q_mul(m1.yy, m2.yw)), q_mul(m1.yz, m2.zw)),
+    }};
 }
 
 // -------------------------------- Q_MAT2X4 -------------------------------- //
 
 static inline Q_MAT2 q_mat2x4_mul_mat4x2(Q_MAT2X4 m1, Q_MAT4X2 m2)
 {
-
+    return (Q_MAT2){{
+        q_add(q_add(q_add(q_mul(m1.xx, m2.xx), q_mul(m1.xy, m2.yx)), q_mul(m1.xz, m2.zx)), q_mul(m1.xw, m2.wx)),
+        q_add(q_add(q_add(q_mul(m1.xx, m2.xy), q_mul(m1.xy, m2.yy)), q_mul(m1.xz, m2.zy)), q_mul(m1.xw, m2.wy)),
+        
+        q_add(q_add(q_add(q_mul(m1.yx, m2.xx), q_mul(m1.yy, m2.yx)), q_mul(m1.yz, m2.zx)), q_mul(m1.yw, m2.wx)),
+        q_add(q_add(q_add(q_mul(m1.yx, m2.xy), q_mul(m1.yy, m2.yy)), q_mul(m1.yz, m2.zy)), q_mul(m1.yw, m2.wy)),
+    }};
 }
 
 static inline Q_MAT2X3 q_mat2x4_mul_mat4x3(Q_MAT2X4 m1, Q_MAT4X3 m2)
 {
-
+    return (Q_MAT2X3){{
+        q_add(q_add(q_add(q_mul(m1.xx, m2.xx), q_mul(m1.xy, m2.yx)), q_mul(m1.xz, m2.zx)), q_mul(m1.xw, m2.wx)),
+        q_add(q_add(q_add(q_mul(m1.xx, m2.xy), q_mul(m1.xy, m2.yy)), q_mul(m1.xz, m2.zy)), q_mul(m1.xw, m2.wy)),
+        q_add(q_add(q_add(q_mul(m1.xx, m2.xz), q_mul(m1.xy, m2.yz)), q_mul(m1.xz, m2.zz)), q_mul(m1.xw, m2.wz)),
+        
+        q_add(q_add(q_add(q_mul(m1.yx, m2.xx), q_mul(m1.yy, m2.yx)), q_mul(m1.yz, m2.zx)), q_mul(m1.yw, m2.wx)),
+        q_add(q_add(q_add(q_mul(m1.yx, m2.xy), q_mul(m1.yy, m2.yy)), q_mul(m1.yz, m2.zy)), q_mul(m1.yw, m2.wy)),
+        q_add(q_add(q_add(q_mul(m1.yx, m2.xz), q_mul(m1.yy, m2.yz)), q_mul(m1.yz, m2.zz)), q_mul(m1.yw, m2.wz)),
+    }};
 }
 
 static inline Q_MAT2X4 q_mat2x4_mul_mat4(Q_MAT2X4 m1, Q_MAT4 m2) 
@@ -93,24 +139,69 @@ static inline Q_MAT2X4 q_mat2x4_mul_mat4(Q_MAT2X4 m1, Q_MAT4 m2)
 
 static inline Q_MAT3X2 q_mat3x2_mul_mat2(Q_MAT3X2 m1, Q_MAT2 m2)
 {
-
+    return (Q_MAT3X2){{
+        q_add(q_mul(m1.xx, m2.xx), q_mul(m1.xy, m2.yx)),
+        q_add(q_mul(m1.xx, m2.xy), q_mul(m1.xy, m2.yy)),
+        
+        q_add(q_mul(m1.yx, m2.xx), q_mul(m1.yy, m2.yx)),
+        q_add(q_mul(m1.yx, m2.xy), q_mul(m1.yy, m2.yy)),
+        
+        q_add(q_mul(m1.zx, m2.xx), q_mul(m1.zy, m2.yx)),
+        q_add(q_mul(m1.zx, m2.xy), q_mul(m1.zy, m2.yy)),
+    }};
 }
 
 static inline Q_MAT3 q_mat3x2_mul_mat2x3(Q_MAT3X2 m1, Q_MAT2X3 m2)
 {
-
+    return (Q_MAT3){{
+        q_add(q_mul(m1.xx, m2.xx), q_mul(m1.xy, m2.yx)),
+        q_add(q_mul(m1.xx, m2.xy), q_mul(m1.xy, m2.yy)),
+        q_add(q_mul(m1.xx, m2.xz), q_mul(m1.xy, m2.yz)),
+        
+        q_add(q_mul(m1.yx, m2.xx), q_mul(m1.yy, m2.yx)),
+        q_add(q_mul(m1.yx, m2.xy), q_mul(m1.yy, m2.yy)),
+        q_add(q_mul(m1.yx, m2.xz), q_mul(m1.yy, m2.yz)),
+        
+        q_add(q_mul(m1.zx, m2.xx), q_mul(m1.zy, m2.yx)),
+        q_add(q_mul(m1.zx, m2.xy), q_mul(m1.zy, m2.yy)),
+        q_add(q_mul(m1.zx, m2.xz), q_mul(m1.zy, m2.yz)),
+    }};
 }
 
 static inline Q_MAT3X4 q_mat3x2_mul_mat2x4(Q_MAT3X2 m1, Q_MAT2X4 m2)
 {
-
+    return (Q_MAT3X4){{
+        q_add(q_mul(m1.xx, m2.xx), q_mul(m1.xy, m2.yx)),
+        q_add(q_mul(m1.xx, m2.xy), q_mul(m1.xy, m2.yy)),
+        q_add(q_mul(m1.xx, m2.xz), q_mul(m1.xy, m2.yz)),
+        q_add(q_mul(m1.xx, m2.xw), q_mul(m1.xy, m2.yw)),
+        
+        q_add(q_mul(m1.yx, m2.xx), q_mul(m1.yy, m2.yx)),
+        q_add(q_mul(m1.yx, m2.xy), q_mul(m1.yy, m2.yy)),
+        q_add(q_mul(m1.yx, m2.xz), q_mul(m1.yy, m2.yz)),
+        q_add(q_mul(m1.yx, m2.xw), q_mul(m1.yy, m2.yw)),
+        
+        q_add(q_mul(m1.zx, m2.xx), q_mul(m1.zy, m2.yx)),
+        q_add(q_mul(m1.zx, m2.xy), q_mul(m1.zy, m2.yy)),
+        q_add(q_mul(m1.zx, m2.xz), q_mul(m1.zy, m2.yz)),
+        q_add(q_mul(m1.zx, m2.xw), q_mul(m1.zy, m2.yw)),
+    }};
 }
 
 // -------------------------------- Q_MAT3 -------------------------------- //
 
 static inline Q_MAT3X2 q_mat3_mul_mat3x2(Q_MAT3 m1, Q_MAT3X2 m2)
 {
-
+    return (Q_MAT3X2){{
+        q_add(q_add(q_mul(m1.xx, m2.xx), q_mul(m1.xy, m2.yx)), q_mul(m1.xz, m2.zx)),
+        q_add(q_add(q_mul(m1.xx, m2.xy), q_mul(m1.xy, m2.yy)), q_mul(m1.xz, m2.zy)),
+        
+        q_add(q_add(q_mul(m1.yx, m2.xx), q_mul(m1.yy, m2.yx)), q_mul(m1.yz, m2.zx)),
+        q_add(q_add(q_mul(m1.yx, m2.xy), q_mul(m1.yy, m2.yy)), q_mul(m1.yz, m2.zy)),
+        
+        q_add(q_add(q_mul(m1.zx, m2.xx), q_mul(m1.zy, m2.yx)), q_mul(m1.zz, m2.zx)),
+        q_add(q_add(q_mul(m1.zx, m2.xy), q_mul(m1.zy, m2.yy)), q_mul(m1.zz, m2.zy)),
+    }};
 }
 
 static inline Q_MAT3 q_mat3_mul_mat3(Q_MAT3 m1, Q_MAT3 m2) 
@@ -132,19 +223,55 @@ static inline Q_MAT3 q_mat3_mul_mat3(Q_MAT3 m1, Q_MAT3 m2)
 
 static inline Q_MAT3X4 q_mat3_mul_mat3x4(Q_MAT3 m1, Q_MAT3X4 m2)
 {
-
+    return (Q_MAT3X4){{
+        q_add(q_add(q_mul(m1.xx, m2.xx), q_mul(m1.xy, m2.yx)), q_mul(m1.xz, m2.zx)),
+        q_add(q_add(q_mul(m1.xx, m2.xy), q_mul(m1.xy, m2.yy)), q_mul(m1.xz, m2.zy)),
+        q_add(q_add(q_mul(m1.xx, m2.xz), q_mul(m1.xy, m2.yz)), q_mul(m1.xz, m2.zz)),
+        q_add(q_add(q_mul(m1.xx, m2.xw), q_mul(m1.xy, m2.yw)), q_mul(m1.xz, m2.zw)),
+        
+        q_add(q_add(q_mul(m1.yx, m2.xx), q_mul(m1.yy, m2.yx)), q_mul(m1.yz, m2.zx)),
+        q_add(q_add(q_mul(m1.yx, m2.xy), q_mul(m1.yy, m2.yy)), q_mul(m1.yz, m2.zy)),
+        q_add(q_add(q_mul(m1.yx, m2.xz), q_mul(m1.yy, m2.yz)), q_mul(m1.yz, m2.zz)),
+        q_add(q_add(q_mul(m1.yx, m2.xw), q_mul(m1.yy, m2.yw)), q_mul(m1.yz, m2.zw)),
+        
+        q_add(q_add(q_mul(m1.zx, m2.xx), q_mul(m1.zy, m2.yx)), q_mul(m1.zz, m2.zx)),
+        q_add(q_add(q_mul(m1.zx, m2.xy), q_mul(m1.zy, m2.yy)), q_mul(m1.zz, m2.zy)),
+        q_add(q_add(q_mul(m1.zx, m2.xz), q_mul(m1.zy, m2.yz)), q_mul(m1.zz, m2.zz)),
+        q_add(q_add(q_mul(m1.zx, m2.xw), q_mul(m1.zy, m2.yw)), q_mul(m1.zz, m2.zw)),
+    }};
 }
 
 // -------------------------------- Q_MAT3X4 -------------------------------- //
 
 static inline Q_MAT3X2 q_mat3x4_mul_mat4x2(Q_MAT3X4 m1, Q_MAT4X2 m2)
 {
-
+    return (Q_MAT3X2){{
+        q_add(q_add(q_add(q_mul(m1.xx, m2.xx), q_mul(m1.xy, m2.yx)), q_mul(m1.xz, m2.zx)), q_mul(m1.xw, m2.wx)),
+        q_add(q_add(q_add(q_mul(m1.xx, m2.xy), q_mul(m1.xy, m2.yy)), q_mul(m1.xz, m2.zy)), q_mul(m1.xw, m2.wy)),
+        
+        q_add(q_add(q_add(q_mul(m1.yx, m2.xx), q_mul(m1.yy, m2.yx)), q_mul(m1.yz, m2.zx)), q_mul(m1.yw, m2.wx)),
+        q_add(q_add(q_add(q_mul(m1.yx, m2.xy), q_mul(m1.yy, m2.yy)), q_mul(m1.yz, m2.zy)), q_mul(m1.yw, m2.wy)),
+        
+        q_add(q_add(q_add(q_mul(m1.zx, m2.xx), q_mul(m1.zy, m2.yx)), q_mul(m1.zz, m2.zx)), q_mul(m1.zw, m2.wx)),
+        q_add(q_add(q_add(q_mul(m1.zx, m2.xy), q_mul(m1.zy, m2.yy)), q_mul(m1.zz, m2.zy)), q_mul(m1.zw, m2.wy)),
+    }};
 }
 
 static inline Q_MAT3 q_mat3x4_mul_mat4x3(Q_MAT3X4 m1, Q_MAT4X3 m2) 
 {
-
+    return (Q_MAT3){{
+        q_add(q_add(q_add(q_mul(m1.xx, m2.xx), q_mul(m1.xy, m2.yx)), q_mul(m1.xz, m2.zx)), q_mul(m1.xw, m2.wx)),
+        q_add(q_add(q_add(q_mul(m1.xx, m2.xy), q_mul(m1.xy, m2.yy)), q_mul(m1.xz, m2.zy)), q_mul(m1.xw, m2.wy)),
+        q_add(q_add(q_add(q_mul(m1.xx, m2.xz), q_mul(m1.xy, m2.yz)), q_mul(m1.xz, m2.zz)), q_mul(m1.xw, m2.wz)),
+        
+        q_add(q_add(q_add(q_mul(m1.yx, m2.xx), q_mul(m1.yy, m2.yx)), q_mul(m1.yz, m2.zx)), q_mul(m1.yw, m2.wx)),
+        q_add(q_add(q_add(q_mul(m1.yx, m2.xy), q_mul(m1.yy, m2.yy)), q_mul(m1.yz, m2.zy)), q_mul(m1.yw, m2.wy)),
+        q_add(q_add(q_add(q_mul(m1.yx, m2.xz), q_mul(m1.yy, m2.yz)), q_mul(m1.yz, m2.zz)), q_mul(m1.yw, m2.wz)),
+        
+        q_add(q_add(q_add(q_mul(m1.zx, m2.xx), q_mul(m1.zy, m2.yx)), q_mul(m1.zz, m2.zx)), q_mul(m1.zw, m2.wx)),
+        q_add(q_add(q_add(q_mul(m1.zx, m2.xy), q_mul(m1.zy, m2.yy)), q_mul(m1.zz, m2.zy)), q_mul(m1.zw, m2.wy)),
+        q_add(q_add(q_add(q_mul(m1.zx, m2.xz), q_mul(m1.zy, m2.yz)), q_mul(m1.zz, m2.zz)), q_mul(m1.zw, m2.wz)),
+    }};
 }
 
 static inline Q_MAT3X4 q_mat3x4_mul_mat4(Q_MAT3X4 m1, Q_MAT4 m2) 
@@ -171,16 +298,65 @@ static inline Q_MAT3X4 q_mat3x4_mul_mat4(Q_MAT3X4 m1, Q_MAT4 m2)
 
 static inline Q_MAT4X2 q_mat4x2_mul_mat2(Q_MAT4X2 m1, Q_MAT2 m2)
 {
-
+    return (Q_MAT4X2){{
+        q_add(q_mul(m1.xx, m2.xx), q_mul(m1.xy, m2.yx)),
+        q_add(q_mul(m1.xx, m2.xy), q_mul(m1.xy, m2.yy)),
+        
+        q_add(q_mul(m1.yx, m2.xx), q_mul(m1.yy, m2.yx)),
+        q_add(q_mul(m1.yx, m2.xy), q_mul(m1.yy, m2.yy)),
+        
+        q_add(q_mul(m1.zx, m2.xx), q_mul(m1.zy, m2.yx)),
+        q_add(q_mul(m1.zx, m2.xy), q_mul(m1.zy, m2.yy)),
+        
+        q_add(q_mul(m1.wx, m2.xx), q_mul(m1.wy, m2.yx)),
+        q_add(q_mul(m1.wx, m2.xy), q_mul(m1.wy, m2.yy)),
+    }};
 }
 
 static inline Q_MAT4X3 q_mat4x2_mul_mat2x3(Q_MAT4X2 m1, Q_MAT2X3 m2)
 {
-
+    return (Q_MAT4X3){{
+        q_add(q_mul(m1.xx, m2.xx), q_mul(m1.xy, m2.yx)),
+        q_add(q_mul(m1.xx, m2.xy), q_mul(m1.xy, m2.yy)),
+        q_add(q_mul(m1.xx, m2.xz), q_mul(m1.xy, m2.yz)),
+        
+        q_add(q_mul(m1.yx, m2.xx), q_mul(m1.yy, m2.yx)),
+        q_add(q_mul(m1.yx, m2.xy), q_mul(m1.yy, m2.yy)),
+        q_add(q_mul(m1.yx, m2.xz), q_mul(m1.yy, m2.yz)),
+        
+        q_add(q_mul(m1.zx, m2.xx), q_mul(m1.zy, m2.yx)),
+        q_add(q_mul(m1.zx, m2.xy), q_mul(m1.zy, m2.yy)),
+        q_add(q_mul(m1.zx, m2.xz), q_mul(m1.zy, m2.yz)),
+        
+        q_add(q_mul(m1.wx, m2.xx), q_mul(m1.wy, m2.yx)),
+        q_add(q_mul(m1.wx, m2.xy), q_mul(m1.wy, m2.yy)),
+        q_add(q_mul(m1.wx, m2.xz), q_mul(m1.wy, m2.yz)),
+    }};
 }
 
 static inline Q_MAT4 q_mat4x2_mul_mat2x4(Q_MAT4X2 m1, Q_MAT2X4 m2)
 {
+    return (Q_MAT4){{
+        q_add(q_mul(m1.xx, m2.xx), q_mul(m1.xy, m2.yx)),
+        q_add(q_mul(m1.xx, m2.xy), q_mul(m1.xy, m2.yy)),
+        q_add(q_mul(m1.xx, m2.xz), q_mul(m1.xy, m2.yz)),
+        q_add(q_mul(m1.xx, m2.xw), q_mul(m1.xy, m2.yw)),
+        
+        q_add(q_mul(m1.yx, m2.xx), q_mul(m1.yy, m2.yx)),
+        q_add(q_mul(m1.yx, m2.xy), q_mul(m1.yy, m2.yy)),
+        q_add(q_mul(m1.yx, m2.xz), q_mul(m1.yy, m2.yz)),
+        q_add(q_mul(m1.yx, m2.xw), q_mul(m1.yy, m2.yw)),
+        
+        q_add(q_mul(m1.zx, m2.xx), q_mul(m1.zy, m2.yx)),
+        q_add(q_mul(m1.zx, m2.xy), q_mul(m1.zy, m2.yy)),
+        q_add(q_mul(m1.zx, m2.xz), q_mul(m1.zy, m2.yz)),
+        q_add(q_mul(m1.zx, m2.xw), q_mul(m1.zy, m2.yw)),
+        
+        q_add(q_mul(m1.wx, m2.xx), q_mul(m1.wy, m2.yx)),
+        q_add(q_mul(m1.wx, m2.xy), q_mul(m1.wy, m2.yy)),
+        q_add(q_mul(m1.wx, m2.xz), q_mul(m1.wy, m2.yz)),
+        q_add(q_mul(m1.wx, m2.xw), q_mul(m1.wy, m2.yw)),
+    }};
 
 }
 
@@ -188,29 +364,105 @@ static inline Q_MAT4 q_mat4x2_mul_mat2x4(Q_MAT4X2 m1, Q_MAT2X4 m2)
 
 static inline Q_MAT4X2 q_mat4x3_mul_mat3x2(Q_MAT4X3 m1, Q_MAT3X2 m2)
 {
-
+    return (Q_MAT4X2){{
+        q_add(q_add(q_mul(m1.xx, m2.xx), q_mul(m1.xy, m2.yx)), q_mul(m1.xz, m2.zx)),
+        q_add(q_add(q_mul(m1.xx, m2.xy), q_mul(m1.xy, m2.yy)), q_mul(m1.xz, m2.zy)),
+        
+        q_add(q_add(q_mul(m1.yx, m2.xx), q_mul(m1.yy, m2.yx)), q_mul(m1.yz, m2.zx)),
+        q_add(q_add(q_mul(m1.yx, m2.xy), q_mul(m1.yy, m2.yy)), q_mul(m1.yz, m2.zy)),
+        
+        q_add(q_add(q_mul(m1.zx, m2.xx), q_mul(m1.zy, m2.yx)), q_mul(m1.zz, m2.zx)),
+        q_add(q_add(q_mul(m1.zx, m2.xy), q_mul(m1.zy, m2.yy)), q_mul(m1.zz, m2.zy)),
+        
+        q_add(q_add(q_mul(m1.wx, m2.xx), q_mul(m1.wy, m2.yx)), q_mul(m1.wz, m2.zx)),
+        q_add(q_add(q_mul(m1.wx, m2.xy), q_mul(m1.wy, m2.yy)), q_mul(m1.wz, m2.zy)),
+    }};
 }
 
 static inline Q_MAT4X3 q_mat4x3_mul_mat3(Q_MAT4X3 m1, Q_MAT3 m2)
 {
-
+    return (Q_MAT4X3){{
+        q_add(q_add(q_mul(m1.xx, m2.xx), q_mul(m1.xy, m2.yx)), q_mul(m1.xz, m2.zx)),
+        q_add(q_add(q_mul(m1.xx, m2.xy), q_mul(m1.xy, m2.yy)), q_mul(m1.xz, m2.zy)),
+        q_add(q_add(q_mul(m1.xx, m2.xz), q_mul(m1.xy, m2.yz)), q_mul(m1.xz, m2.zz)),
+        
+        q_add(q_add(q_mul(m1.yx, m2.xx), q_mul(m1.yy, m2.yx)), q_mul(m1.yz, m2.zx)),
+        q_add(q_add(q_mul(m1.yx, m2.xy), q_mul(m1.yy, m2.yy)), q_mul(m1.yz, m2.zy)),
+        q_add(q_add(q_mul(m1.yx, m2.xz), q_mul(m1.yy, m2.yz)), q_mul(m1.yz, m2.zz)),
+        
+        q_add(q_add(q_mul(m1.zx, m2.xx), q_mul(m1.zy, m2.yx)), q_mul(m1.zz, m2.zx)),
+        q_add(q_add(q_mul(m1.zx, m2.xy), q_mul(m1.zy, m2.yy)), q_mul(m1.zz, m2.zy)),
+        q_add(q_add(q_mul(m1.zx, m2.xz), q_mul(m1.zy, m2.yz)), q_mul(m1.zz, m2.zz)),
+        
+        q_add(q_add(q_mul(m1.wx, m2.xx), q_mul(m1.wy, m2.yx)), q_mul(m1.wz, m2.zx)),
+        q_add(q_add(q_mul(m1.wx, m2.xy), q_mul(m1.wy, m2.yy)), q_mul(m1.wz, m2.zy)),
+        q_add(q_add(q_mul(m1.wx, m2.xz), q_mul(m1.wy, m2.yz)), q_mul(m1.wz, m2.zz)),
+    }};
 }
 
 static inline Q_MAT4 q_mat4x3_mul_mat3x4(Q_MAT4X3 m1, Q_MAT3X4 m2)
 {
-
+    return (Q_MAT4){{
+        q_add(q_add(q_mul(m1.xx, m2.xx), q_mul(m1.xy, m2.yx)), q_mul(m1.xz, m2.zx)),
+        q_add(q_add(q_mul(m1.xx, m2.xy), q_mul(m1.xy, m2.yy)), q_mul(m1.xz, m2.zy)),
+        q_add(q_add(q_mul(m1.xx, m2.xz), q_mul(m1.xy, m2.yz)), q_mul(m1.xz, m2.zz)),
+        q_add(q_add(q_mul(m1.xx, m2.xw), q_mul(m1.xy, m2.yw)), q_mul(m1.xz, m2.zw)),
+        
+        q_add(q_add(q_mul(m1.yx, m2.xx), q_mul(m1.yy, m2.yx)), q_mul(m1.yz, m2.zx)),
+        q_add(q_add(q_mul(m1.yx, m2.xy), q_mul(m1.yy, m2.yy)), q_mul(m1.yz, m2.zy)),
+        q_add(q_add(q_mul(m1.yx, m2.xz), q_mul(m1.yy, m2.yz)), q_mul(m1.yz, m2.zz)),
+        q_add(q_add(q_mul(m1.yx, m2.xw), q_mul(m1.yy, m2.yw)), q_mul(m1.yz, m2.zw)),
+        
+        q_add(q_add(q_mul(m1.zx, m2.xx), q_mul(m1.zy, m2.yx)), q_mul(m1.zz, m2.zx)),
+        q_add(q_add(q_mul(m1.zx, m2.xy), q_mul(m1.zy, m2.yy)), q_mul(m1.zz, m2.zy)),
+        q_add(q_add(q_mul(m1.zx, m2.xz), q_mul(m1.zy, m2.yz)), q_mul(m1.zz, m2.zz)),
+        q_add(q_add(q_mul(m1.zx, m2.xw), q_mul(m1.zy, m2.yw)), q_mul(m1.zz, m2.zw)),
+        
+        q_add(q_add(q_mul(m1.wx, m2.xx), q_mul(m1.wy, m2.yx)), q_mul(m1.wz, m2.zx)),
+        q_add(q_add(q_mul(m1.wx, m2.xy), q_mul(m1.wy, m2.yy)), q_mul(m1.wz, m2.zy)),
+        q_add(q_add(q_mul(m1.wx, m2.xz), q_mul(m1.wy, m2.yz)), q_mul(m1.wz, m2.zz)),
+        q_add(q_add(q_mul(m1.wx, m2.xw), q_mul(m1.wy, m2.yw)), q_mul(m1.wz, m2.zw)),
+    }};
 }
 
 // -------------------------------- Q_MAT4 -------------------------------- //
 
 static inline Q_MAT4X2 q_mat4_mul_mat4x2(Q_MAT4 m1, Q_MAT4X2 m2)
 {
-
+    return (Q_MAT4X2){{
+        q_add(q_add(q_add(q_mul(m1.xx, m2.xx), q_mul(m1.xy, m2.yx)), q_mul(m1.xz, m2.zx)), q_mul(m1.xw, m2.wx)),
+        q_add(q_add(q_add(q_mul(m1.xx, m2.xy), q_mul(m1.xy, m2.yy)), q_mul(m1.xz, m2.zy)), q_mul(m1.xw, m2.wy)),
+        
+        q_add(q_add(q_add(q_mul(m1.yx, m2.xx), q_mul(m1.yy, m2.yx)), q_mul(m1.yz, m2.zx)), q_mul(m1.yw, m2.wx)),
+        q_add(q_add(q_add(q_mul(m1.yx, m2.xy), q_mul(m1.yy, m2.yy)), q_mul(m1.yz, m2.zy)), q_mul(m1.yw, m2.wy)),
+        
+        q_add(q_add(q_add(q_mul(m1.zx, m2.xx), q_mul(m1.zy, m2.yx)), q_mul(m1.zz, m2.zx)), q_mul(m1.zw, m2.wx)),
+        q_add(q_add(q_add(q_mul(m1.zx, m2.xy), q_mul(m1.zy, m2.yy)), q_mul(m1.zz, m2.zy)), q_mul(m1.zw, m2.wy)),
+        
+        q_add(q_add(q_add(q_mul(m1.wx, m2.xx), q_mul(m1.wy, m2.yx)), q_mul(m1.wz, m2.zx)), q_mul(m1.ww, m2.wx)),
+        q_add(q_add(q_add(q_mul(m1.wx, m2.xy), q_mul(m1.wy, m2.yy)), q_mul(m1.wz, m2.zy)), q_mul(m1.ww, m2.wy)),
+    }};
 }
 
 static inline Q_MAT4X3 q_mat4_mul_mat4x3(Q_MAT4 m1, Q_MAT4X3 m2)
 {
-
+    return (Q_MAT4X3){{
+        q_add(q_add(q_add(q_mul(m1.xx, m2.xx), q_mul(m1.xy, m2.yx)), q_mul(m1.xz, m2.zx)), q_mul(m1.xw, m2.wx)),
+        q_add(q_add(q_add(q_mul(m1.xx, m2.xy), q_mul(m1.xy, m2.yy)), q_mul(m1.xz, m2.zy)), q_mul(m1.xw, m2.wy)),
+        q_add(q_add(q_add(q_mul(m1.xx, m2.xz), q_mul(m1.xy, m2.yz)), q_mul(m1.xz, m2.zz)), q_mul(m1.xw, m2.wz)),
+        
+        q_add(q_add(q_add(q_mul(m1.yx, m2.xx), q_mul(m1.yy, m2.yx)), q_mul(m1.yz, m2.zx)), q_mul(m1.yw, m2.wx)),
+        q_add(q_add(q_add(q_mul(m1.yx, m2.xy), q_mul(m1.yy, m2.yy)), q_mul(m1.yz, m2.zy)), q_mul(m1.yw, m2.wy)),
+        q_add(q_add(q_add(q_mul(m1.yx, m2.xz), q_mul(m1.yy, m2.yz)), q_mul(m1.yz, m2.zz)), q_mul(m1.yw, m2.wz)),
+        
+        q_add(q_add(q_add(q_mul(m1.zx, m2.xx), q_mul(m1.zy, m2.yx)), q_mul(m1.zz, m2.zx)), q_mul(m1.zw, m2.wx)),
+        q_add(q_add(q_add(q_mul(m1.zx, m2.xy), q_mul(m1.zy, m2.yy)), q_mul(m1.zz, m2.zy)), q_mul(m1.zw, m2.wy)),
+        q_add(q_add(q_add(q_mul(m1.zx, m2.xz), q_mul(m1.zy, m2.yz)), q_mul(m1.zz, m2.zz)), q_mul(m1.zw, m2.wz)),
+        
+        q_add(q_add(q_add(q_mul(m1.wx, m2.xx), q_mul(m1.wy, m2.yx)), q_mul(m1.wz, m2.zx)), q_mul(m1.ww, m2.wx)),
+        q_add(q_add(q_add(q_mul(m1.wx, m2.xy), q_mul(m1.wy, m2.yy)), q_mul(m1.wz, m2.zy)), q_mul(m1.ww, m2.wy)),
+        q_add(q_add(q_add(q_mul(m1.wx, m2.xz), q_mul(m1.wy, m2.yz)), q_mul(m1.wz, m2.zz)), q_mul(m1.ww, m2.wz)),
+    }};
 }
 
 static inline Q_MAT4 q_mat4_mul_mat4(Q_MAT4 m1, Q_MAT4 m2) 
--- matrix/q_mat_tr_template.h
+++ matrix/q_mat_tr_template.h
@@ -12,8 +12,6 @@
 #include "q_mat4x3_template.h"
 #include "q_mat4_template.h"
 
-// TODO: implement functions
-
 static inline Q_MAT2 q_mat2_tr(Q_MAT2 m) 
 {
     return (Q_MAT2){{
@@ -33,12 +31,20 @@ static inline Q_MAT3X2 q_mat2x3_tr(Q_MAT2X3 m)
 
 static inline Q_MAT4X2 q_mat2x4_tr(Q_MAT2X4 m)
 {
-
+    return (Q_MAT4X2){{
+        m.xx, m.yx,
+        m.xy, m.yy,
+        m.xz, m.yz,
+        m.xw, m.yw,
+    }};
 }
 
 static inline Q_MAT2X3 q_mat3x2_tr(Q_MAT3X2 m)
 {
-
+    return (Q_MAT2X3){{
+        m.xx, m.yx, m.zx,
+        m.xy, m.yy, m.zy,
+    }};
 }
 
 static inline Q_MAT3 q_mat3_tr(Q_MAT3 m) 
@@ -52,17 +58,29 @@ static inline Q_MAT3 q_mat3_tr(Q_MAT3 m)
 
 static inline Q_MAT4X3 q_mat3x4_tr(Q_MAT3X4 m)
 {
-
+    return (Q_MAT4X3){{
+        m.xx, m.yx, m.zx,
+        m.xy, m.yy, m.zy,
+        m.xz, m.yz, m.zz,
+        m.xw, m.yw, m.zw,
+    }};
 }
 
 static inline Q_MAT2X4 q_mat4x2_tr(Q_MAT4X2 m)
 {
-
+    return (Q_MAT2X4){{
+        m.xx, m.yx, m.zx, m.wx,
+        m.xy, m.yy, m.zy, m.wy,
+    }};
 }
 
 static inline Q_MAT3X4 q_mat4x3_tr(Q_MAT4X3 m)
 {
-
+    return (Q_MAT3X4){{
+        m.xx, m.yx, m.zx, m.wx,
+        m.xy, m.yy, m.zy, m.wy,
+        m.xz, m.yz, m.zz, m.wz,
+    }};
 }
 
 static inline Q_MAT4 q_mat4_tr(Q_MAT4 m) 
